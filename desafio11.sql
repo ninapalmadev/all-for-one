dcce57ab4997c6f40d60b1ea5cf22532b9a12e67
Nina Palma <ninapalmadev@gmail.com>
2023-08-17 16:24:07 -0300

    requisito 11 e corrige chamada de db

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,3 @@
-SELECT notes FROM northwind.purchase_orders
+SELECT notes FROM purchase_orders
 WHERE notes >= 'Purchase generated based on Order #30' 
 AND notes <= 'Purchase generated based on Order #39';
